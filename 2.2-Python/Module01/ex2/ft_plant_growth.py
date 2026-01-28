a16c47fb5f02dc14007c09b8f770e763aeab4205
Mohamed Eldemiry <meldemir@c0c2c1.42berlin.de>
2026-01-28 17:39:23 +0100

    python mod1

--- 2.2-Python/Module01/ex2/ft_plant_growth.py
+++ 2.2-Python/Module01/ex2/ft_plant_growth.py
@@ -5,27 +5,27 @@ class Plant:
         self.height = height
         self.current_age = age
 
-    def ft_get_info(self):
+    def display_info(self):
         print(f"{self.name}: {self.height}cm, {self.current_age} days old")
 
     def grow(self, cm: int = 1) -> None:
         self.height += cm
 
-    def age(self):
+    def age(self) -> None:
         self.current_age += 1
 
 if __name__ == "__main__":
     
     p1 = Plant("Rose", 25, 30)
     print("=== Day 1 ===")
-    p1.ft_get_info()
+    p1.display_info()
     old_height = p1.height
     for i in range(1,7):
         p1.grow()
         p1.age()
 
     print("=== Day 7 ===")
-    p1.ft_get_info()
+    p1.display_info()
     new_height = p1.height
     grow_height = new_height - old_height
     print(f"Growth this week: +{grow_height}cm")
--- 2.2-Python/Module01/ex4/ft_garden_security.py
+++ 2.2-Python/Module01/ex4/ft_garden_security.py
@@ -2,21 +2,40 @@
 class SecurePlant:
     def __init__(self, name:str, height:int, age:int):
         self.name = name
-        self.height = height
-        self.current_age = age
-        self.ft_get_info()
+        self._height = height
+        self._age = age
+        print(f"Plant created: {self.name}")
 
-    def ft_get_info(self):
-        print(f"Created: {self.name} ({self.height}cm, {self.current_age} days)")
-
+    def display_info(self) -> None:
+        print(f"Current plant: {self.name} ({self._height}cm, {self._age} days)")
+    
+    def set_height(self, new_height:int):
+        if new_height >= 0:
+            self._height = new_height
+            print(f"Height updated: {self._height}cm [OK]")
+        else:
+            print(f"Invalid operation attempted: height {new_height}cm [REJECTED]")
+            print("Security: Negative height rejected")
+            
+    def set_age(self, new_age:int) -> None:
+        if new_age >= 0:
+            self._age = new_age
+            print(f"Age updated: {self._age}cm [OK]")
+        else:
+            print(f"Invalid operation attempted: age {new_age}days [REJECTED]")
+            print("Security: Negative age rejected")
+            
+    def get_height(self) -> int:
+        return self._height
+    
+    def get_age(self) -> int:
+        return self._age
+        
 if __name__ == "__main__":
-    print("=== Plant Factory Output ===")
-    plants = [
-        Plant("Rose", 25, 30),
-        Plant("Oak", 200, 365),
-        Plant("Cactus", 15, 120),
-        Plant("Sunflower", 80, 45),
-        Plant("Fern", 15, 120)
-    ]
-    print(f"Total plants created: {len(plants)}")
+    print("=== Garden Security System ===")
+    plant1 = SecurePlant("Rose", 25, 30)
+    plant1.set_height(25)
+    plant1.set_age(30)
+    plant1.set_height(-5)
+    plant1.display_info()
     
